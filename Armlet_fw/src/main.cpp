65066a079d20c2daea7a9c2ec6d02acd5d4a5f09
Leonid Tsarev <leo@bastilia.ru>
2013-09-06 14:50:34 +0400

    v1.7 works, allocaton stll not optimized

--- Armlet_fw/src/main.cpp
+++ Armlet_fw/src/main.cpp
@@ -49,20 +49,20 @@ int main() {
     if(ClkResult) Uart.Printf("Clock failure\r");
 
     while(TRUE) {
-        //chThdSleepMilliseconds(999);
-        chSysLock();
-        chSchGoSleepS(THD_STATE_SUSPENDED); // Forever
-        chSysUnlock();
+        chThdSleepMilliseconds(999);
+//        chSysLock();
+//        chSchGoSleepS(THD_STATE_SUSPENDED); // Forever
+//        chSysUnlock();
 
         // Ctrl-Alt-Del
-//        if(!PinIsSet(KEY_GPIO, 3) and !PinIsSet(KEY_GPIO, 4) and !PinIsSet(KEY_GPIO, 9)) {
-//            REBOOT();
-//        }
+        if(!PinIsSet(KEY_GPIO, 3) and !PinIsSet(KEY_GPIO, 4) and !PinIsSet(KEY_GPIO, 9)) {
+            REBOOT();
+        }
     }
 }
 
 void Init() {
-    Uart.Init(256000);
+    Uart.Init(115200);
     Uart.Printf("Armlet3\r");
     SD.Init();
     // Read config
@@ -82,6 +82,6 @@ void Init() {
     PillInit();
 //    Sound.Init();
 //    Sound.Play("alive.wav");
-//    rLevel1.Init(ID);
+    rLevel1.Init(ID);
     AppInit();
 }
