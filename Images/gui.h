ede32027a4764897f74028f7b0dc8cc998b2b069
Vasily Zakharov <vmzakhar@gmail.com>
2014-03-26 07:08:45 +0400

    followup

--- Images/gui.h
+++ Images/gui.h
@@ -17,29 +17,29 @@
 #define BUTTON_ENABLED 2
 #define BUTTON_DISABLED 3
 
-#define BUTTONS 'ABCLERXYZ'
+#define BUTTONS "ABCLERXYZ"
 
-#define NO_BUTTON {null, 0, 0, null, null, null}
+#define NO_BUTTON {0, 0, 0, 0, 0, 0}
 
 typedef struct Button {
     char name;
     const int left;
     const int bottom;
     // All function arguments are screen number and button number, may be ignored.
-    bool (*isPressable)(int, int); // Called if pressed, must return true if pressable, false otherwise.
+    int (*isPressable)(int, int); // Called if pressed, must return non-zero if pressable, zero otherwise.
     int (*getState)(int, int); // Called at redrawing, must return button state.
     int (*press)(int, int); // Called after pressed, must process pressing and return new button state.
 } Button_t;
 
 typedef struct Screen {
     const char* name;
-    const Button_t buttons[];
+    const Button_t buttons[sizeof(BUTTONS)];
 } Screen_t;
 
 extern Screen_t screens[];
 extern const int screens_number;
 
-extern bool buttonIsPressable(int, int);
+extern int buttonIsPressable(int, int);
 extern int buttonGetState(int, int);
 extern int buttonPress(int, int);
 
