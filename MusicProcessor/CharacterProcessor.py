03eadceb7420538470291822717723bf505fec4f
Vasily Zakharov <vmzakhar@gmail.com>
2014-06-05 04:29:46 +0400

    First draft of CharacterProcessor.py

---
+++ MusicProcessor/CharacterProcessor.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#
+# Character Processor for Ticket to Atlantis LARP.
+#
+# Usage:
+# -
+#
+from csv import reader as CSVReader, writer as CSVWriter
+from os.path import isfile
+
+from allrpg import getAllRoles
+
+GAME_ID = 584
+
+NAME_COLUMN = 29
+
+CHARACTER_ID_START = 101
+
+CHARACTERS_CSV = 'Characters.csv'
+
+CHARACTERS_CSV_HEADER = '''\
+#
+# Characters.csv
+#
+# Character name -> RID table for ArmLet initialization.
+#
+# Generated, updated and used by CharacterProcessor.py to track persistent and unique character ArmLet IDs.
+#
+# !!! DO NOT EDIT !!!
+#
+'''
+
+def readCSV(csv): # generator
+    with open(csv) as f:
+        for row in CSVReader(f):
+            assert row, "bad CSV file format"
+            if not row[0].startswith('#'):
+                yield row
+
+def verifyCharacters(characters):
+    assert tuple(sorted(characters.itervalues())) == tuple(xrange(CHARACTER_ID_START, CHARACTER_ID_START + len(characters))), "Damaged %s file" % CHARACTERS_CSV
+
+def readCharacters(fileName = CHARACTERS_CSV):
+    if not isfile(fileName):
+        return {}
+    ret = dict((name, int(number)) for (name, number) in readCSV(fileName))
+    verifyCharacters(ret)
+    return ret
+
+def writeCharacters(characters, fileName = CHARACTERS_CSV, header = CHARACTERS_CSV_HEADER):
+    with open(fileName, 'wb') as f:
+        f.writelines(s + '\n' for s in header.splitlines())
+        CSVWriter(f).writerows(sorted(characters.iteritems(), key = lambda (name, number): number))
+
+def loadCharacters():
+    print "Fetching data from allrpg.info..."
+    allRoles = getAllRoles(GAME_ID)
+    print "Done"
+    # ToDo: Employ some sorting? By creation date?
+    return tuple(str(name) for name in (allRoles[row][NAME_COLUMN] for row in xrange(1, len(allRoles))) if name)
+
+def updateCharacters():
+    characters = readCharacters()
+    for name in loadCharacters():
+        if name not in characters:
+            characters[name] = len(characters) + CHARACTER_ID_START
+    verifyCharacters(characters)
+    writeCharacters(characters)
+
+def main():
+    updateCharacters()
+
+if __name__ == '__main__':
+    main()
---
+++ MusicProcessor/allrpg.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#
+# allrpg.info API
+#
+# Requires xlrd package from https://pypi.python.org/pypi/xlrd
+# On Windows, a newer version of sqlite3.dll must be downloaded from http://sqlite.org/download.html and put to C:\Python27\DLLs
+#
+from cookielib import Cookie, LWPCookieJar
+from os import listdir
+from os.path import expanduser, getmtime, isdir, join
+from platform import system
+from sqlite3 import connect
+from urllib2 import build_opener, HTTPCookieProcessor, Request
+
+try:
+    from xlrd import open_workbook
+except ImportError, ex:
+    raise ImportError("%s: %s\n\nPlease install xlrd v0.9.3 or later: https://pypi.python.org/pypi/xlrd\n" % (ex.__class__.__name__, ex))
+
+FIREFOX_PROFILES_LINUX = '~/.mozilla/firefox'
+FIREFOX_PROFILES_WINDOWS = r'~\Application Data\Mozilla\Firefox\Profiles'
+FIREFOX_COOKIE_FILE = 'cookies.sqlite'
+FIREFOX_COOKIES_SQL_REQUEST = "SELECT host, path, isSecure, expiry, name, value FROM moz_cookies WHERE host LIKE '%%%s%%'"
+
+def getFirefoxCookies(domain = ''):
+    """Returns Mozilla Firefox cookies for the specified domain."""
+    isWindows = system().lower().startswith('win')
+    profilesPath = unicode(expanduser(FIREFOX_PROFILES_WINDOWS if isWindows else FIREFOX_PROFILES_LINUX))
+    profilesFiles = (join(profilesPath, f) for f in listdir(profilesPath))
+    profilesDirs = (f for f in profilesFiles if isdir(f) and f.endswith('.default'))
+    profileDir = sorted(profilesDirs, key = getmtime)[0]
+    cookieDB = join(profileDir, FIREFOX_COOKIE_FILE)
+    cursor = connect(cookieDB).cursor()
+    cursor.execute(FIREFOX_COOKIES_SQL_REQUEST % domain)
+    cookieJar = LWPCookieJar()
+    for (host, path, isSecure, expiry, name, value) in cursor.fetchall():
+        cookieJar.set_cookie(Cookie(0, name, value, None, False,
+            host, host.startswith('.'), host.startswith('.'),
+            path, False, isSecure, expiry, not expiry, None, None, {}))
+    return cookieJar
+
+COOKIE_SOURCES = {'firefox': getFirefoxCookies}
+
+COOKIE_DOMAIN = 'allrpg.info'
+
+ALL_ROLES_URL = 'http://site.allrpg.info/orders/site=%d/action=export-xlsx-single'
+
+def loadWithCookies(url, cookieJar, userAgent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'):
+    """Returns a document download from the specified URL, accessed with specified cookies."""
+    opener = build_opener(HTTPCookieProcessor(cookieJar))
+    return opener.open(Request(url, None, {'User-Agent' : userAgent})).read()
+
+def getAllRoles(gameID, cookieSource = 'firefox'):
+    """Returns all roles data for a game with specified ID."""
+    cookieJar = COOKIE_SOURCES[cookieSource.lower()](COOKIE_DOMAIN)
+    allRolesXLS = loadWithCookies(ALL_ROLES_URL % gameID, cookieJar)
+    xls = open_workbook(file_contents = allRolesXLS)
+    sheet = xls.sheets()[0]
+    return tuple(tuple(cell.value for cell in sheet.row(row)) for row in xrange(sheet.nrows))
