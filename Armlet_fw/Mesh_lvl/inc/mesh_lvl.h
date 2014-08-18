f9bd27fa42e13e5ae8dfd72ffad3582bac219d78
Roman Leonov <r.leonov@wwpass.com>
2014-08-18 19:09:43 +0400

    Change mesh params and FIX GetAstronomicTime in mesh.

--- Armlet_fw/Mesh_lvl/inc/mesh_lvl.h
+++ Armlet_fw/Mesh_lvl/inc/mesh_lvl.h
@@ -168,7 +168,7 @@ public:
     void UpdateSleepTime()              { SleepTime = ((App.ID-1)*SLOT_TIME); }
     uint32_t GetCycleN()                { return (AbsCycle);             }
     uint32_t GetAbsTimeMS()             { return (AbsCycle*CYCLE_TIME);  }
-    uint8_t GetAstronomicTime(char *PToStr);
+    uint8_t GetAstronomicTime(char *PToStr, uint8_t MaxLen);
     int32_t SetNewAbsCycleN(uint32_t ANew)   {
         int32_t Diff = AbsCycle;
         AbsCycle = ANew;
