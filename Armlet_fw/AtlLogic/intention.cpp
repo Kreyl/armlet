36105af13932f1ac9ace5923cb6169586c7258bb
Kozin Filipp <filippk83@yandex.ru>
2014-09-11 10:02:33 +0400

    ограничил winning_integral чтобы избежать  int  переполнения

--- Armlet_fw/AtlLogic/intention.cpp
+++ Armlet_fw/AtlLogic/intention.cpp
@@ -546,6 +546,10 @@ void CalculateIntentionsRadioChange() {
             if(SICD.last_intention_index_winner != ArrayOfIncomingIntentions[0].reason_indx) SICD.winning_integral=0;
 
             SICD.winning_integral+=GetNotNormalizedIntegral(ArrayOfIncomingIntentions[0].power512,ArrayOfIncomingIntentions[0].reason_indx)/SICD.Normalizer;
+            if(SICD.winning_integral>25000)
+                SICD.winning_integral=25000)
+        if(SICD.winning_integral<0)
+            SICD.winning_integral=0)
             SICD.last_intention_power_winner=ArrayOfIncomingIntentions[0].power512;//get current power!
             SICD.last_intention_index_winner=ArrayOfIncomingIntentions[0].reason_indx;
             return;
