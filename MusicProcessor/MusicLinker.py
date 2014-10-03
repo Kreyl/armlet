4d513455d3585004dfa7e2a5ace7f86588c52a34
Vasily Zakharov <vmzakhar@gmail.com>
2014-10-03 05:53:38 +0400

    followup

--- MusicProcessor/MusicLinker.py
+++ MusicProcessor/MusicLinker.py
@@ -11,12 +11,12 @@ from sys import stdout, argv
 stdout = fdopen(stdout.fileno(), 'w', 0)
 
 try: # Filesystem symbolic links configuration
-    from os import link, symlink # UNIX # pylint: disable=E0611, W0611
+    from os import link as hardlink, symlink # UNIX # pylint: disable=E0611, W0611
 except ImportError:
     try:
         from ctypes import windll
         dll = windll.LoadLibrary('kernel32.dll')
-        def link(source, linkName):
+        def hardlink(source, linkName):
             if not dll.CreateHardLinkW(linkName, source, None):
                 raise OSError("code %d" % dll.GetLastError())
         def symlink(source, linkName):
@@ -60,7 +60,7 @@ def main(*args):
             emotions[emotion].add(linkName)
             fullLinkName = join(emotionDir, linkName)
             if not isfile(fullLinkName) and not islink(fullLinkName):
-                link(fullName, fullLinkName)
+                hardlink(fullName, fullLinkName)
         print
 
 if __name__ == '__main__':
