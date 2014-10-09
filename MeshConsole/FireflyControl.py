fb8a7bf2b0d2072de075a205b3ef4580f8d6a0a5
Vasily Zakharov <v.zakharov@wwpass.com>
2014-10-09 20:52:17 +0400

    FireflyControl split into two files

--- MeshConsole/FireflyControl.py
+++ MeshConsole/FireflyControl.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 #
-# Firefly Control
+# Firefly Control main module
 #
 from collections import deque
 from getopt import getopt
-from itertools import chain, islice
 from logging import getLogger, getLoggerClass, setLoggerClass, FileHandler, Formatter, Handler, INFO, NOTSET
 from os.path import basename
 from sys import argv, exit # pylint: disable=W0622
@@ -12,18 +11,16 @@ from traceback import format_exc
 
 try:
     from PyQt4 import uic
-    from PyQt4.QtCore import Qt, QCoreApplication, QDateTime, QObject, QSettings, QSize, pyqtSignal
-    from PyQt4.QtGui import QApplication, QDesktopWidget, QColorDialog, QDialog, QFileDialog, QMainWindow
-    from PyQt4.QtGui import QHBoxLayout, QScrollArea, QStackedWidget, QWidget
-    from PyQt4.QtGui import QButtonGroup, QIcon, QLabel, QLineEdit, QRadioButton, QToolButton
-    from PyQt4.QtGui import QColor, QIntValidator, QSizePolicy
+    from PyQt4.QtCore import Qt, QCoreApplication, QDateTime, QObject, QSettings, pyqtSignal
+    from PyQt4.QtGui import QApplication, QDesktopWidget, QDialog, QFileDialog, QLabel, QLineEdit, QMainWindow, QScrollArea
 except ImportError, ex:
     raise ImportError("%s: %s\n\nPlease install PyQt4 v4.10.4 or later: http://riverbankcomputing.com/software/pyqt/download\n" % (ex.__class__.__name__, ex))
 
 from UARTTextProtocol import Command, COMMAND_MARKER
 from UARTTextCommands import ackResponse, ffGetCommand, ffSetCommand, ffResponse
-from UARTTextCommands import UART_FF_RGB, UART_FF_WAIT, UART_FF_GOTO, UART_FF_LIGHT
 from SerialPort import SerialPort, DT, TIMEOUT
+
+from FireflyWidgets import CommandWidget, InsertCommandButton
 
 LONG_DATETIME_FORMAT = 'yyyy.MM.dd hh:mm:ss'
 
@@ -50,11 +47,6 @@ WINDOW_TITLE = '%s :: %s%s'
 WINDOW_SIZE = 2.0 / 3
 WINDOW_POSITION = (1 - WINDOW_SIZE) / 2
 
-MAX_INT = 2 ** 31 - 1
-
-MIN_COLOR = 0
-MAX_COLOR = 255
-
 #
 # ToDo:
 # - Optimize resize events with sizes updating
@@ -62,17 +54,6 @@ MAX_COLOR = 255
 # - Hide "fade in" for first command, and "stay for" for last one (if there's no loop)
 # - Set LED color immediately when color is selected in GUI
 #
-
-def fixWidgetSize(widget, adjustment = 1):
-    widget.setFixedWidth(widget.fontMetrics().boundingRect(widget.text()).width() * adjustment) # This is a bad hack, but there's no better idea
-
-def setTip(widget, tip):
-    widget.setToolTip(tip)
-    widget.setStatusTip(tip)
-
-def widgets(layout, headerSize = 0, tailSize = 0): # generator
-    for i in xrange(headerSize, layout.count() - tailSize):
-        yield layout.itemAt(i).widget()
 
 class CallableHandler(Handler):
     def __init__(self, emitCallable, level = NOTSET):
@@ -140,395 +121,6 @@ class AboutDialog(QDialog):
     def __init__(self):
         QDialog.__init__(self)
         uic.loadUi(ABOUT_UI_FILE_NAME, self)
-
-class SelectColorLabel(QLabel):
-    def __init__(self, parent, callback = None, color = None):
-        QLabel.__init__(self, parent)
-        self.setFrameStyle(self.Box | self.Plain)
-        setTip(self, "Select color")
-        self.callback = callback
-        self.setColor(color or QColor(Qt.white))
-        self.mousePressEvent = self.editColor
-
-    def setCorrectSize(self, size):
-        size -= 2 * self.lineWidth()
-        if size > 0:
-            self.setFixedSize(size, size)
-
-    def setColor(self, color):
-        self.color = color
-        self.setStyleSheet('background-color: %s' % color.name())
-        if self.callback:
-            self.callback(color)
-
-    def editColor(self, _event):
-        previousColor = self.color
-        colorDialog = QColorDialog(self.color)
-        colorDialog.currentColorChanged.connect(self.setColor)
-        if not colorDialog.exec_():
-            self.setColor(previousColor)
-
-class TimeEdit(QLineEdit):
-    MAX_VALUE = MAX_INT
-    MAX_LENGTH = len(str(MAX_VALUE))
-
-    def __init__(self, parent, callback = None, initialValue = None):
-        QLineEdit.__init__(self, parent)
-        self.setAlignment(Qt.AlignRight)
-        self.setMaxLength(self.MAX_LENGTH)
-        self.setValidator(QIntValidator(0, self.MAX_VALUE))
-        self.setText(str(self.MAX_VALUE))
-        fixWidgetSize(self, 1.4)
-        self.setText(str(initialValue or 0))
-        if callback:
-            self.textEdited.connect(callback)
-
-class DeleteButton(QToolButton):
-    def __init__(self, parent, callback):
-        QToolButton.__init__(self, parent)
-        self.setIcon(QIcon('images/delete.png'))
-        setTip(self, "Delete command")
-        self.clicked.connect(callback)
-
-    def setCorrectSize(self, size):
-        self.setFixedSize(size, size)
-
-class CommandWidget(QWidget):
-    BASIC_STYLESHEET = 'border: 1px solid'
-    GRADIENT_TEMPLATE = 'background: qlineargradient(x1:0, x2:1, %s)'
-    GRADIENT_STOP = 'stop: %f rgb(%d, %d, %d)'
-
-    commandsLayout = None
-    correctHeight = None
-    sizeDidntChange = None
-
-    @classmethod
-    def configure(cls, stackedWidget, parentWidget, updateProgramCallback):
-        cls.stackedWidget = stackedWidget
-        cls.parentWidget = parentWidget
-        cls.updateProgramCallback = updateProgramCallback
-        cls.commandsLayout = parentWidget.layout()
-        cls.headerWidget = widgets(cls.commandsLayout).next()
-        cls.headerLayout = cls.headerWidget.layout()
-        cls.buttonGroup = QButtonGroup(parentWidget)
-        cls.TAIL_SIZE = 1
-        cls.HEADER_SIZE = cls.commandsLayout.count() - cls.TAIL_SIZE
-
-    @classmethod
-    def updateDeleteButtons(cls):
-        hiderIndexToSet = cls.numCommands() < 2
-        for command in cls.commands():
-            command.hider.setCurrentIndex(hiderIndexToSet)
-
-    @classmethod
-    def updateLoop(cls, programChanged = True):
-        for command in cls.commands():
-            command.setLoopIcon()
-        cls.updateProgram(programChanged)
-
-    @classmethod
-    def updateProgram(cls, programChanged = True):
-        commands = []
-        programLength = 1.0
-        for (rgb, morphLength, delayLength, _command) in (c.command() for c in chain(cls.commands(), cls.loopCommands())):
-            commands.append((rgb, morphLength or 1, delayLength or 1))
-            programLength += (morphLength or 1) + (delayLength or 1)
-        dx = programLength / 1000
-        for (rgb, morphLength, delayLength) in commands:
-            programLength += max(0, dx - morphLength) + max(0, dx - delayLength)
-        stops = [(0, 0, 0, 0),]
-        position = 0.0
-        for (rgb, morphLength, delayLength) in commands:
-            position += max(morphLength, dx)
-            stops.append((position / programLength,) + rgb)
-            position += max(delayLength, dx)
-            stops.append((position / programLength,) + rgb)
-        cls.updateProgramCallback(','.join(c.command()[-1] for c in cls.commands()),
-                '; '.join((cls.BASIC_STYLESHEET, cls.GRADIENT_TEMPLATE % ' '.join(cls.GRADIENT_STOP % stop for stop in stops))),
-                programChanged)
-
-    @classmethod
-    def setupProgram(cls, source = None):
-        def parseInt(words, index, minValue = 0, maxValue = MAX_INT):
-            try:
-                ret = float(words[index])
-                if ret % 1:
-                    raise ValueError()
-                ret = int(ret)
-                if not minValue <= ret <= maxValue:
-                    raise ValueError()
-            except ValueError:
-                raise ValueError("Bad program data at word %d: expected a %d..%d integer" % (index, minValue, maxValue))
-            return ret
-
-        def parseColor(words, index):
-            return parseInt(words, index, MIN_COLOR, MAX_COLOR)
-
-        words = []
-        if source is not None:
-            for line in source:
-                words.extend(w.lower() for w in (w.strip() for w in line.strip().split('#')[0].split(',')) if w)
-            if not words:
-                raise ValueError("No commands found in the file")
-        commands = []
-        index = 0
-        numCommands = 0
-        gotoPosition = None
-        conditionsPresent = False
-        positionsToVerify = []
-        while index < len(words):
-            if words[index] == UART_FF_RGB:
-                if len(words) < index + 5:
-                    raise ValueError("Abrupt program end")
-                index += 1
-                r = parseColor(words, index)
-                index += 1
-                g = parseColor(words, index)
-                index += 1
-                b = parseColor(words, index)
-                index += 1
-                morphLength = parseInt(words, index)
-                commands.append([r, g, b, morphLength, 0, numCommands])
-            elif words[index] == UART_FF_WAIT:
-                if len(words) < index + 2:
-                    raise ValueError("Abrupt program end")
-                if not commands:
-                    commands.append([0, 0, 0, 0, 0, 0])
-                index += 1
-                commands[-1][4] += parseInt(words, index)
-            elif words[index] == UART_FF_GOTO:
-                if len(words) < index + 2:
-                    raise ValueError("Abrupt program end")
-                if conditionsPresent:
-                    index += 1
-                    positionsToVerify.append(index)
-                else:
-                    if len(words) > index + 2:
-                        raise ValueError("Unexpected program data at word %d after GOTO command" % (index + 2))
-                    if not commands:
-                        raise ValueError("Unexpected GOTO command at the start of the program")
-                    index += 1
-                    gotoPosition = parseInt(words, index, 0, numCommands - 1)
-            elif words[index] == UART_FF_LIGHT:
-                if len(words) < index + 3:
-                    raise ValueError("Abrupt program end")
-                index += 1
-                _light = parseInt(words, index)
-                index += 1
-                positionsToVerify.append(index)
-                conditionsPresent = True
-            numCommands += 1
-            index += 1
-        for index in positionsToVerify:
-            parseInt(words, index, 0, numCommands - 1) # verify jump targets
-        if not conditionsPresent and gotoPosition:
-            for (index, command) in enumerate(commands):
-                if command[-1] == gotoPosition:
-                    command[-1] = True
-                    break
-                if command[-1] > gotoPosition:
-                    raise ValueError("GOTO command targeted to a non-color command")
-        cls.stackedWidget.setCurrentIndex(conditionsPresent)
-        if conditionsPresent:
-            cls.updateProgramCallback(','.join(words), cls.BASIC_STYLESHEET, False)
-        else:
-            for command in tuple(cls.commands()):
-                command.delete(False)
-            if commands:
-                gotoCommand = None
-                for (r, g, b, morphLength, delayLength, gotoHere) in commands:
-                    commandWidget = CommandWidget(QColor(r, g, b), morphLength, delayLength, False)
-                    if gotoHere is True:
-                        gotoCommand = commandWidget
-                    InsertCommandButton()
-                if gotoCommand:
-                    gotoCommand.radioButton.setChecked(True)
-            else:
-                CommandWidget(programChanged = False)
-                InsertCommandButton()
-            cls.updateLoop(False) # ToDo: Make this the only program update
-            cls.updateDeleteButtons()
-
-    @classmethod
-    def adjustSizes(cls, size):
-        if cls.sizeDidntChange < 2: # This is a hack, but I couldn't find a better way to coordinate technically independent layouts
-            cls.correctHeight = size
-            sampleLayout = cls.commands().next().layout()
-            cls.headerLayout.setContentsMargins(sampleLayout.contentsMargins())
-            widthChanged = False
-            for (h, w) in zip(widgets(cls.headerLayout), widgets(sampleLayout)):
-                if h.width() != w.width():
-                    widthChanged = True
-                    h.setFixedWidth(w.width())
-            if widthChanged:
-                cls.sizeDidntChange = 0
-            else:
-                cls.sizeDidntChange += 1
-        for command in cls.commands():
-            command.setCorrectSize()
-        InsertCommandButton.adjustSizes(size, cls.headerWidget.height() + cls.commandsLayout.spacing())
-
-    def __init__(self, color = None, morphLength = None, delayLength = None, index = None, programChanged = True):
-        QWidget.__init__(self, self.parentWidget)
-        self.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
-        self.colorLabel = SelectColorLabel(self, self.setColor, color)
-        self.morphEdit = TimeEdit(self, self.updateProgram, morphLength)
-        self.delayEdit = TimeEdit(self, self.updateProgram, delayLength)
-        self.radioButton = QRadioButton(self)
-        self.radioButton.clicked.connect(self.updateLoop)
-        self.radioButton.commandWidget = self
-        self.buttonGroup.addButton(self.radioButton)
-        self.deleteButton = DeleteButton(self, self.delete)
-        self.hider = QStackedWidget(self)
-        self.hiderWidget = QWidget(self)
-        hiderLayout = QHBoxLayout(self.hiderWidget)
-        hiderLayout.setContentsMargins(0, 0, 0, 0)
-        hiderLayout.addWidget(self.radioButton)
-        hiderLayout.addWidget(self.deleteButton)
-        self.hider.addWidget(self.hiderWidget)
-        self.hider.addWidget(QWidget(self))
-        layout = QHBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.colorLabel)
-        layout.addWidget(self.morphEdit)
-        layout.addWidget(self.delayEdit)
-        layout.addWidget(self.hider)
-        for (h, w) in zip(widgets(self.headerLayout), widgets(layout)):
-            if not w.toolTip() and hasattr(h, 'text'):
-                setTip(w, h.text())
-        self.setCorrectSize()
-        lastIndex = self.commandsLayout.count() - self.TAIL_SIZE
-        insertIndex = index + self.HEADER_SIZE if index != None else lastIndex
-        if not self.buttonGroup.checkedButton() or insertIndex == lastIndex and not self.hasLoop():
-            self.radioButton.setChecked(True)
-        self.commandsLayout.insertWidget(insertIndex, self)
-        self.updateLoop(programChanged)
-        self.updateDeleteButtons()
-
-    @classmethod
-    def numCommands(cls):
-        return cls.commandsLayout.count() - cls.HEADER_SIZE - cls.TAIL_SIZE
-
-    @classmethod
-    def commands(cls): # generator
-        for w in widgets(cls.commandsLayout, cls.HEADER_SIZE, cls.TAIL_SIZE):
-            yield w
-
-    @classmethod
-    def loopCommands(cls): # generator
-        if not cls.hasLoop():
-            return
-        for w in widgets(cls.commandsLayout, cls.loopEndIndex(), cls.TAIL_SIZE):
-            yield w
-
-    @classmethod
-    def loopEndIndex(cls):
-        return cls.commandsLayout.indexOf(cls.buttonGroup.checkedButton().commandWidget)
-
-    def isFirst(self):
-        return self.commandsLayout.indexOf(self) == self.HEADER_SIZE
-
-    def isLast(self):
-        return self.commandsLayout.indexOf(self) == self.commandsLayout.count() - 1 - self.TAIL_SIZE
-
-    def isLoopStart(self):
-        return self.isLast()
-
-    def isLoopEnd(self):
-        return self.radioButton.isChecked()
-
-    def isInLoop(self):
-        return self.commandsLayout.indexOf(self) >= self.loopEndIndex()
-
-    @classmethod
-    def hasLoop(cls):
-        return cls.buttonGroup.checkedButton() and not cls.buttonGroup.checkedButton().commandWidget.isLast()
-
-    def setLoopIcon(self):
-        self.radioButton.setIcon(QIcon('images/%s.png' % ( \
-                ('noloop' if self.isLoopStart() else 'loopend') if self.isLoopEnd() else \
-                'loopstart' if self.isLoopStart() else 'inloop' if self.isInLoop() else 'outofloop')))
-
-    def resizeEvent(self, event):
-        self.adjustSizes(self.setCorrectSize())
-        QWidget.resizeEvent(self, event)
-
-    def setCorrectSize(self):
-        size = max(self.correctHeight, self.morphEdit.height())
-        self.colorLabel.setCorrectSize(size)
-        self.deleteButton.setCorrectSize(size)
-        self.radioButton.setFixedWidth(size + self.radioButton.icon().actualSize(QSize(100, 100)).width())
-        return size
-
-    def setColor(self, color):
-        self.color = color
-        self.updateProgram()
-
-    def delete(self, programChanged = True):
-        if self.isLoopEnd():
-            index = self.commandsLayout.indexOf(self)
-            index = index - 1 if self.isLast() else index + 1
-            if index >= self.HEADER_SIZE:
-                self.commandsLayout.itemAt(index).widget().radioButton.setChecked(True)
-        self.buttonGroup.removeButton(self.radioButton)
-        self.setParent(None)
-        self.updateLoop(programChanged)
-        self.updateDeleteButtons()
-        InsertCommandButton.deleteExtraButtons(self.numCommands())
-
-    def command(self):
-        fields = [UART_FF_RGB, self.color.red(), self.color.green(), self.color.blue(), self.morphEdit.text()]
-        if int(self.delayEdit.text()):
-            fields.extend((UART_FF_WAIT, self.delayEdit.text()))
-        if self.isLoopStart() and not self.isLoopEnd():
-            targetIndex = 0
-            for command in islice(self.commands(), 0, self.loopEndIndex() - self.HEADER_SIZE):
-                targetIndex += 1 + bool(int(command.delayEdit.text()))
-            fields.extend((UART_FF_GOTO, targetIndex))
-        return ((self.color.red(), self.color.green(), self.color.blue()),
-                int(self.morphEdit.text()), int(self.delayEdit.text()), ','.join(str(f) for f in fields))
-
-class InsertCommandButton(QToolButton):
-    insertCommandLayout = None
-    correctHeight = None
-
-    @classmethod
-    def configure(cls, parentWidget):
-        cls.parentWidget = parentWidget
-        cls.insertCommandLayout = parentWidget.layout()
-        cls.TAIL_SIZE = 1
-        cls.HEADER_SIZE = cls.insertCommandLayout.count() - cls.TAIL_SIZE
-
-    @classmethod
-    def deleteExtraButtons(cls, count):
-        for button in widgets(cls.insertCommandLayout, cls.HEADER_SIZE + count + 1, cls.TAIL_SIZE):
-            button.setParent(None)
-
-    @classmethod
-    def adjustSizes(cls, lineSize, headerSize):
-        if lineSize > cls.correctHeight:
-            cls.correctHeight = lineSize
-            cls.insertCommandLayout.setContentsMargins(0, headerSize - lineSize // 2, 0, 0)
-            for button in widgets(cls.insertCommandLayout, cls.HEADER_SIZE, cls.TAIL_SIZE):
-                button.setCorrectSize(lineSize)
-
-    def __init__(self):
-        QToolButton.__init__(self, self.parentWidget)
-        self.setIcon(QIcon('images/add.png'))
-        setTip(self, "Insert command")
-        self.index = self.insertCommandLayout.count() - self.TAIL_SIZE
-        self.clicked.connect(self.addCommand)
-        self.insertCommandLayout.insertWidget(self.index, self)
-        self.setCorrectSize()
-
-    def setCorrectSize(self, size = None):
-        size = size or self.correctHeight
-        if size:
-            self.setFixedHeight(size)
-
-    def addCommand(self):
-        CommandWidget(index = self.index)
-        InsertCommandButton()
 
 class FireflyControl(QMainWindow):
     comConnect = pyqtSignal(str)
