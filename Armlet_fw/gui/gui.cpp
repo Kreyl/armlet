7266913a2e0c13b1f54c79d7a22c1e4c60bc49c2
Roman Leonov <r.leonov@wwpass.com>
2014-10-20 22:33:00 +0400

    Integral done.

--- Armlet_fw/gui/gui.cpp
+++ Armlet_fw/gui/gui.cpp
@@ -20,41 +20,134 @@ void gui_t::Init() {
     draw_SelfID();
     draw_SelfName();
     Lcd.DrawBatteryBounds();
+
 }
 
-void gui_t::draw_RxTable(RxTable_t *P) {
-    clean_RxTable();
-//    if(P->PTable->Size == 0) {
-//        draw_EmptyLine(REASON_NAME_START_Y, clOrangeClr, clBlackClr);
+void gui_t::RxTableParse(RxTable_t *P) {
+    for(uint8_t vert_str=0; vert_str < P->PTable->Size; vert_str++) {
+        if((P->PTable->Row[vert_str].ID >= PERSON_ID_START) && (PERSON_ID_END >= P->PTable->Row[vert_str].ID)) {
+            NeighbourTbl.PutNeighbor(P->PTable->Row[vert_str].ID, P->PTable->Row[vert_str].Level);
+        } // if reason is human number
+    } // for string in RxTable
+    NeighbourTbl.PrepareDisplayBuf();
+    NeighbourTbl.DisplayNeighbors();
+}
+
+
+#if 1 // Integlral Neighbours
+void NeighbourTbl_t::PutNeighbor(uint16_t ID, uint8_t Level) {
+    // check if we got this ID
+    Uart.Printf("\rPut %u", ID);
+    if(LineCnt < MAX_NEIGHBOR_SIZE) { NeighbourBuf[LineCnt].Put(ID, Level); LineCnt++; }
+}
+
+void NeighbourTbl_t::PrepareDisplayBuf() {
+    SortByPower();
+    Uart.Printf("\rLineCnt %u", LineCnt);
+    CleanUpDisplayBuf();
+    if(LineCnt > GUI_NEIGHBOR_LINES) { // All data is freshy
+        Uart.Printf("\r All freshen");
+        for(uint8_t i=0; i<GUI_NEIGHBOR_LINES; i++) {
+            DisplayBuf[i] = NeighbourBuf[i];
+            DisplayBufSz = GUI_NEIGHBOR_LINES;
+        }
+    }
+    else { // should check the data and fill the display information correctly
+        NeighbourLine_t *P = NeighbourBuf;
+        while(LineCnt-- > 0) {
+            uint8_t Line = WRONG_PARAM;
+            Uart.Printf("\r Fetch %u",  P->ID);
+            for(uint8_t i=0; i<DisplayBufSz; i++) {
+                if(P->ID == DisplayBuf[i].ID) Line = i;
+            }
+            Uart.Printf("\r");
+            for(uint8_t i=0; i<DisplayBufSz; i++) Uart.Printf(" %u", DisplayBuf[i].ID);
+            Uart.Printf("\r");
+
+            if(Line != WRONG_PARAM) {
+                Uart.Printf(" present");
+                DisplayBuf[Line] = *P++;
+            }
+            else if(DisplayBufSz < GUI_NEIGHBOR_LINES) {
+                    Uart.Printf(" newcoming");
+                    DisplayBuf[DisplayBufSz] = *P++;
+                    DisplayBufSz++;
+            } else {
+                // Find more relevant information
+                Uart.Printf(" replace");
+                Line = FindPlace();
+                if(Line != WRONG_PARAM) DisplayBuf[Line] = *P++;
+            }
+        } // while LineCnt
+    }
+    Uart.Printf("\rDisplayBuf (%u): ", DisplayBufSz);
+    for(uint8_t i=0; i<DisplayBufSz; i++) Uart.Printf(" %u:%u ", DisplayBuf[i].ID, DisplayBuf[i].LifeTime);
+    Uart.Printf("\r");
+}
+//    if(LineCntToPrint == 0) { NeighbourBuf[LineCntToPrint].Put(ID, Level); LineCntToPrint++; }
+//    else if(LineCntToPrint < MAX_NEIGHBOR_SIZE) {
+//        uint8_t CntToPut = WRONG_PARAM;
+//        for(uint8_t i=0; i<LineCntToPrint; i++) if(NeighbourBuf[i].ID == ID) CntToPut = i;
+//        if(CntToPut != WRONG_PARAM) NeighbourBuf[CntToPut].Put(ID, Level);
+//        else { NeighbourBuf[LineCntToPrint].Put(ID, Level); LineCntToPrint++; }
 //    }
 //    else {
-        for(uint8_t vert_str=0; vert_str < P->PTable->Size; vert_str++) {
-            if((P->PTable->Row[vert_str].ID >= PERSON_ID_START) && (PERSON_ID_END >= P->PTable->Row[vert_str].ID)) {
-                if(RxTable_lines < MAX_GUI_NEIGHBOR_LINES) {
-                    draw_Line(REASON_NAME_START_Y + (RxTable_lines*REASON_NAME_HEIGHT), clOrangeClr, clBlackClr, P->PTable->Row[vert_str].ID, P->PTable->Row[vert_str].Level);
-                    RxTable_lines++;
-                } // if empty line present
-            } // if reason is human number
-        } // for string in RxTable
+//        uint8_t LineNum = GetNotRequaredLineNum();
+//        if(LineNum != WRONG_PARAM) NeighbourBuf[LineNum].Put(ID, Level);
 //    }
-}
+//    LineCntToDelete = LineCntToPrint;
 
-void gui_t::clean_RxTable() {
-    if(RxTable_lines != 0) {
-        do {
-            RxTable_lines--;
-            erase_Line(REASON_NAME_START_Y + (RxTable_lines*REASON_NAME_HEIGHT), clOrangeClr, clBlackClr);
-        } while(RxTable_lines != 0);
+void NeighbourTbl_t::DisplayNeighbors() {
+    Uart.Printf("\rCntToPrint %u", DisplayBufSz);
+    Cls();
+    if(DisplayBufSz == 0) {
+        GUI.draw_EmptyLine(REASON_NAME_START_Y, clOrangeClr, clBlack);
     }
-    draw_EmptyLine(REASON_NAME_START_Y, clOrangeClr, clBlackClr);
+    else {
+        for(uint8_t i=0; i<DisplayBufSz; i++) {
+            GUI.draw_Line(REASON_NAME_START_Y + (i*REASON_NAME_HEIGHT), clOrangeClr, clBlack, DisplayBuf[i].ID, DisplayBuf[i].Level);
+            DisplayBuf[i].LifeTime += 1;
+            DisplayBuf[i].Level = 0;
+        }
+    }
+    LineCntToDelete = DisplayBufSz;
+    LineCnt = 0;
 }
 
+void NeighbourTbl_t::Cls() {
+    Uart.Printf("\rCntToDelete %u", LineCntToDelete);
+    for(uint8_t i=0; i<LineCntToDelete; i++) GUI.erase_Line(REASON_NAME_START_Y + (i*REASON_NAME_HEIGHT), clOrangeClr, clBlack);
+}
+        /*
+        Uart.Printf("\rStr ");
+        for(uint8_t i =0; i< Cnt; i++) Uart.Printf(" %u:%u ", NeighbourBuf[i].ID, NeighbourBuf[i].LifeTime);
+        Uart.Printf("\r");
+        uint8_t tmpCnt = Cnt;
+        for (uint8_t i = 0; i < tmpCnt; i++) {
+            NeighbourBuf[i].LifeTime--; // decrease LifeTime
+            if(NeighbourBuf[i].LifeTime == 0) {
+                Uart.Printf("\rInvalid %u", NeighbourBuf[i].ID);
+                for(uint8_t j=i; j<Cnt-1; j++) {
+                    NeighbourBuf[j] = NeighbourBuf[j+1];
+                }
+                Cnt--;
+            } // if LifeTime == 0
+            else GUI.draw_Line(REASON_NAME_START_Y + (i*REASON_NAME_HEIGHT), clOrangeClr, clBlack, NeighbourBuf[i].ID, NeighbourBuf[i].Level);
+            NeighbourBuf[i].Level = 0;
+        } // for Cnt
+        Uart.Printf("\rStr ");
+        for(uint8_t i =0; i< Cnt; i++) Uart.Printf(" %u:%u ", NeighbourBuf[i].ID, NeighbourBuf[i].LifeTime);
+        Uart.Printf("\r");
+    }
+}
+*/
+#endif
 void gui_t::draw_Location(uint8_t LocAddr, uint8_t Power) {
     if(LocAddr == 0) {
         draw_EmptyLine(LOCATION_START_Y, clWhiteClr, clBlackClr);
         return;
     }
-    draw_Location(LOCATION_START_Y, clWhiteClr, clBlackClr, LocAddr, Power);
+    draw_locationI(LOCATION_START_Y, clWhiteClr, clBlackClr, LocAddr, Power);
 }
 
 void gui_t::draw_Time() {
